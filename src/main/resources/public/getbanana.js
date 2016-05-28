333819765d30eb69525c7bbcf39ac73772f46e58
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-05-29 00:04:24 +0200

    Showing torskblock when backend is dead

--- src/main/resources/public/getbanana.js
+++ src/main/resources/public/getbanana.js
@@ -1,9 +1,17 @@
 $(function () {
+
+    function updateMonkey(bananaColor) {
+        $("#monkey").removeClass().addClass(bananaColor);
+    }
+
     window.setInterval(function () {
             $.get("/banana", function (data) {
                 banana = JSON.parse(data);
-                $("#monkey").removeClass().addClass(banana.color);
-              })
+                updateMonkey(banana.color);
+              }
+            ).fail(function() {
+                    updateMonkey("torskblock")
+                });
         },
         1000);
 
