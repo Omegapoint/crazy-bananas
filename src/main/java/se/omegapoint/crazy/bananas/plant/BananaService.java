4d13ebefa954838fcd71288dd3a84c99ff1ae337
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-05-28 20:45:32 +0200

    Separated WaterService too

--- src/main/java/se/omegapoint/crazy/bananas/plant/BananaService.java
+++ src/main/java/se/omegapoint/crazy/bananas/plant/BananaService.java
@@ -4,25 +4,17 @@ package se.omegapoint.crazy.bananas.plant;
  * Created by piolin on 13/05/16.
  */
 
-import com.google.gson.Gson;
-import org.apache.commons.io.IOUtils;
 import se.omegapoint.crazy.bananas.JsonTransformer;
-import se.omegapoint.crazy.bananas.source.DropOfWater;
+import se.omegapoint.crazy.bananas.source.WaterClient;
 import se.omegapoint.crazy.bananas.sun.SunClient;
 import spark.Spark;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
 
 import static spark.Spark.staticFileLocation;
 
 public class BananaService {
 
-    private static final Gson gson = new Gson();
     private static final SunClient sunClient = new SunClient();
+    private static final WaterClient waterClient = new WaterClient();
 
     public static void main(String[] args) {
 
@@ -31,22 +23,7 @@ public class BananaService {
     }
 
     private static Object getBanana() {
-        URI waterUri = URI.create("http://localhost:1723/water");
-
-        try {
-            String water = getResource(waterUri);
-
-            return new Banana(gson.fromJson(water, DropOfWater.class), sunClient.sunShine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new Banana();
+        return new Banana(waterClient.dropOfWater(), sunClient.sunShine());
     }
 
-    private static String getResource(URI uri) throws IOException {
-        URLConnection connection = uri.toURL().openConnection();
-        connection.connect();
-        InputStream stream = connection.getInputStream();
-        return IOUtils.toString(stream, Charset.defaultCharset());
-    }
 }
---
+++ src/main/java/se/omegapoint/crazy/bananas/source/WaterClient.java
@@ -0,0 +1,32 @@
+package se.omegapoint.crazy.bananas.source;
+
+import com.google.gson.Gson;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
+/**
+ * Created by piolin on 28/05/16.
+ */
+public class WaterClient {
+
+    private final Gson gson = new Gson();
+    private final static java.net.URI URI = java.net.URI.create("http://localhost:1723/water");
+
+    public DropOfWater dropOfWater() {
+
+        try {
+            URLConnection connection = URI.toURL().openConnection();
+            connection.connect();
+            InputStream stream = connection.getInputStream();
+            return gson.fromJson(IOUtils.toString(stream, Charset.defaultCharset()), DropOfWater.class);
+        }
+        catch (IOException e) {
+            System.out.println("Totally unexpected when fetching some water: " + e);
+        }
+        return null;
+    }
+}
