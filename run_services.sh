34a6918d93377a108e9860fcc0728bbd4b938839
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-05-29 15:59:50 +0200

    Added one more water service

--- run_services.sh
+++ run_services.sh
@@ -5,4 +5,5 @@ mvn package
 ./run_service.sh "secret.SecretService" 1111
 ./run_service.sh "sun.SunService" 2222
 ./run_service.sh "source.WaterService" 3333
+./run_service.sh "source.WaterService" 3334
 ./run_service.sh "plant.BananaService" 4444
