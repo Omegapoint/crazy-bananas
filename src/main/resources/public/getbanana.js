9597ef4f8dae2146a80bd0fda3bc22f469dc10eb
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-05-30 16:38:56 +0200

    First step towards changing monkey temper based on banana history

--- src/main/resources/public/getbanana.js
+++ src/main/resources/public/getbanana.js
@@ -1,16 +1,31 @@
 $(function () {
 
-    function updateMonkey(bananaColor) {
+    var buffer = [];
+    var bufferPosition = 0;
+    var bufferSize = 10;
+
+    function temper() {
+        return "disappointed";
+    }
+
+    function updateMonkey() {
+        $("#monkey").removeClass().addClass(temper());
+    }
+
+    function updateBanana(bananaColor) {
         $("#banana").removeClass().addClass(bananaColor);
     }
 
     window.setInterval(function () {
             $.get("/banana", function (data) {
-                banana = JSON.parse(data);
-                updateMonkey(banana.color);
+                    banana = JSON.parse(data);
+                    buffer[bufferPosition] = banana.color;
+                    bufferPosition = (bufferPosition + 1) % bufferSize;
+                    updateBanana(banana.color);
+                    updateMonkey();
               }
             ).fail(function() {
-                    updateMonkey("torskblock")
+                    updateBanana("torskblock")
                 });
         },
         1000);
