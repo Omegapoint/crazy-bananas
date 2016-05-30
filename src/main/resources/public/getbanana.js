3db9a6284c1cdab06a1b813e151ac819c2141315
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-05-30 17:07:10 +0200

    Updating monkey temper based on bananas past

--- src/main/resources/public/getbanana.js
+++ src/main/resources/public/getbanana.js
@@ -1,11 +1,29 @@
 $(function () {
 
-    var buffer = [];
+    var happy_lower_limit = 18;
+    var disappointed_lower_limit = 10;
+    var buffer = ["yellow","yellow","yellow","yellow","yellow","yellow","yellow","yellow","yellow","yellow"];
     var bufferPosition = 0;
     var bufferSize = 10;
+    var score = {"yellow": 2, "brown": 1, "torskblock": 0};
+
+    function evaluate(sum) {
+        if (sum >= happy_lower_limit) {
+            return "happy";
+        }
+        if (sum >= disappointed_lower_limit) {
+            return "disappointed";
+        }
+        return "angry";
+    }
 
     function temper() {
-        return "disappointed";
+        var sum = 0;
+        $.each(buffer, function(index, value){
+            sum += score[value];
+        });
+        console.log(sum);
+        return evaluate(sum);
     }
 
     function updateMonkey() {
@@ -14,19 +32,19 @@ $(function () {
 
     function updateBanana(bananaColor) {
         $("#banana").removeClass().addClass(bananaColor);
+        buffer[bufferPosition] = bananaColor;
+        bufferPosition = (bufferPosition + 1) % bufferSize;
     }
 
     window.setInterval(function () {
             $.get("/banana", function (data) {
                     banana = JSON.parse(data);
-                    buffer[bufferPosition] = banana.color;
-                    bufferPosition = (bufferPosition + 1) % bufferSize;
                     updateBanana(banana.color);
-                    updateMonkey();
               }
             ).fail(function() {
                     updateBanana("torskblock")
                 });
+            updateMonkey();
         },
         1000);
 
