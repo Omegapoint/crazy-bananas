11de297eb4e9bb34ed77583ac933a8259b00a9de
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-06-03 11:44:20 +0200

    corrected single service kill script too

--- kill_service.sh
+++ kill_service.sh
@@ -2,6 +2,6 @@
 
 SERVICE="$1"
 
-PIDS=$(ps -e | grep -v grep | grep -v bash | grep "$SERVICE" | cut -d ' ' -f 1)
+PIDS=$(ps -e | grep -v grep | grep -v bash | grep "$SERVICE" | sed -e 's/^[ \t]*//' | cut -d ' ' -f 1)
 
 kill $PIDS
